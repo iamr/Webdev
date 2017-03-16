f8cef224388d34e2a8bf1b3a23342457770621d5
i am r <ik@i-am-r.nl>
2017-03-16 13:35:07 +0100

    Plugins
    Divi builder & Login WP

---
+++ wp-content/plugins/login-social/templates/admin/licensing.php
@@ -0,0 +1,36 @@
+<p><?php _e( 'You are using the Lite version of the Huge-IT Login for WordPress. If you want to get more awesome options, advanced features, settings to customize every area of the plugin, then check out the Full License plugin. The full version of the plugin is available in 3 different packages of one-time payment.', 'hg_login' ); ?></p>
+<a href="https://goo.gl/O6Y1p4" target="_blank" class="button-primary"><?php _e( 'Purchase a License', 'hg_login' ); ?></a>
+<div class="login">
+	<div class="login-block">
+		<div class="licens">
+			<a href="https://goo.gl/O6Y1p4" target="_blank">
+				<span class="icon access"></span>
+				<span class="text"><?php _e( 'Login Menu Access', 'hg_login' ); ?></span>
+			</a>
+		</div>
+		<div class="licens">
+			<a href="https://goo.gl/O6Y1p4" target="_blank">
+				<span class="icon design"></span>
+				<span class="text"><?php _e( 'Button Design', 'hg_login' ); ?></span>
+			</a>
+		</div>
+		<div class="licens">
+			<a href="https://goo.gl/O6Y1p4" target="_blank">
+				<span class="icon option"></span>
+				<span class="text"><?php _e( 'Pop-Up Options', 'hg_login' ); ?></span>
+			</a>
+		</div>
+		<div class="licens">
+			<a href="https://goo.gl/O6Y1p4" target="_blank">
+				<span class="icon recaptcha"></span>
+				<span class="text"><?php _e( 'reCAPTCHA', 'hg_login' ); ?></span>
+			</a>
+		</div>
+	</div>
+</div>
+<p>After the purchasing the Full version follow this steps:</p>
+<ol>
+    <li>Deactivate Huge IT login Plugin</li>
+    <li>Delete Huge IT Login Plugin</li>
+    <li>Install the downloaded Full version of the plugin</li>
+</ol>
